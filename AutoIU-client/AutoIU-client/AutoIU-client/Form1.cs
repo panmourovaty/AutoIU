56e85206bb2eb98dab06ab60a6ed970991f8fbca
panmourovaty <35844848+panmourovaty@users.noreply.github.com>
2020-10-31 18:42:24 +0100

    cmd is now visible

--- AutoIU-client/AutoIU-client/AutoIU-client/Form1.cs
+++ AutoIU-client/AutoIU-client/AutoIU-client/Form1.cs
@@ -16,11 +16,11 @@ namespace AutoIU_client
             var cmd1 = new ProcessStartInfo();
             string Command1;
             cmd1.UseShellExecute = true;
-            Command1 = @"WMIC computersystem where caption='"+Environment.MachineName+@"' rename "+textBox1.Text+ @" && netdom /domain:"+textBox3.Text+@" /user:"+textBox2.Text+@" /password:"+textBox4.Text+@" member "+textBox1.Text+@" /joindomain";
+            Command1 = @"echo off && color 0c && WMIC computersystem where caption='"+Environment.MachineName+@"' rename "+textBox1.Text+ @" && netdom /domain:"+textBox3.Text+@" /user:"+textBox2.Text+@" /password:"+textBox4.Text+@" member "+textBox1.Text+ @" /joindomain && timeout /T 20 && shutdown /r";
             cmd1.FileName = @"cmd.exe";
             cmd1.Verb = "runas";
             cmd1.Arguments = "/c " + Command1;
-            cmd1.WindowStyle = ProcessWindowStyle.Hidden;
+            cmd1.WindowStyle = ProcessWindowStyle.Maximized;
             Process.Start(cmd1);
         }
     }
